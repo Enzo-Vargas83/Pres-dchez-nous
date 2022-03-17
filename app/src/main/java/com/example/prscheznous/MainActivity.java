180f7ed6a24b98a9f7207c8758abc32acd89c430
Vargas Enzo <enzo.vargas@etu.univ-amu.fr>
2022-03-17 15:27:33 +0100

    appli tel accueil

--- app/src/main/java/com/example/prscheznous/MainActivity.java
+++ app/src/main/java/com/example/prscheznous/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.prscheznous;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                //intent.putExtra("key", "val");
+                startActivity(intent);
+            }
+        });
     }
 }
