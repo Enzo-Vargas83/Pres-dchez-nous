4db1f6c3da8b9a9bf4b1c5edda011212d1fdc4ac
Vargas Enzo <enzo.vargas@etu.univ-amu.fr>
2022-03-17 16:38:29 +0100

    menu + nouvelle onglet

--- app/src/main/java/com/example/prscheznous/MainActivity.java
+++ app/src/main/java/com/example/prscheznous/MainActivity.java
@@ -13,16 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ListActivity.class);
-                //intent.putExtra("key", "val");
-                startActivity(intent);
-            }
-        });
     }
 }
